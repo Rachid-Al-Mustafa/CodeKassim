2bf37abe3d227f6b082579ac12044ad30574d735
Your Name <ibrahimkassim975@gmail.com>
2025-02-23 21:35:52 +0200

    build for deploy

--- src/pages/user/Home/Home.tsx
+++ src/pages/user/Home/Home.tsx
@@ -4,7 +4,6 @@ import { BannerSlider } from "../../../generalComponents";
 
 // Import images using `import * as`
 import * as banner1 from "../../../assets/Images/Banner1.jpeg";
-import * as banner2 from "../../../assets/Images/Banner2.jpg"; // Use .jpg if the file is saved as .jpg
 import * as banner3 from "../../../assets/Images/Banner3.jpeg";
 import * as banner4 from "../../../assets/Images/Banner4.jpeg";
 
@@ -14,11 +13,6 @@ const slides: BannerSlide[] = [
     imageUrl: banner1.default, // Access the `default` property
     title: "Big Sale - Up to 50% Off!",
     alt: "Sale Banner",
-  },
-  {
-    imageUrl: banner2.default, // Access the `default` property
-    title: "New Arrivals This Season",
-    alt: "New Arrivals",
   },
   {
     imageUrl: banner3.default, // Access the `default` property
